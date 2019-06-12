558a8592dec2e212cc8cf86a2e520b55a4376780
Kaisei Sunaga <jkdwhssk@yahoo.co.jp>
2019-06-12 19:22:30 +0900

    Add libraries

---
+++ Portal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portal
+//
+//  Created by 砂賀開晴 on 2019/06/11.
+//  Copyright © 2019 Kaisei Sunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
